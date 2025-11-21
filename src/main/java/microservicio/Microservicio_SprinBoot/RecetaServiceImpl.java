13c747b90032cd2a5ff6ba6d3312313cd31da1cc
IsaKatMM <isabel.morocho@unl.edu.ec>
2025-11-21 10:22:42 -0500

    commit

--- src/main/java/microservicio/Microservicio_SprinBoot/RecetaServiceImpl.java
+++ src/main/java/microservicio/Microservicio_SprinBoot/RecetaServiceImpl.java
@@ -11,4 +11,28 @@ public class RecetaServiceImpl implements RecetaService {
     public List<Recetas> obtenerRecetas() {
         return JsonUtil.leerRecetas();
     }
+
+    @Override
+    public Recetas obtenerPorId(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'obtenerPorId'");
+    }
+
+    @Override
+    public Recetas crearReceta(Recetas receta) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'crearReceta'");
+    }
+
+    @Override
+    public Recetas actualizarReceta(Long id, Recetas recetaActualizada) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actualizarReceta'");
+    }
+
+    @Override
+    public void eliminarReceta(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eliminarReceta'");
+    }
 }
